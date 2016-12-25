de6352fe54364b5f419759483a2135db1df7c095
Nathan Panchal <nathanpanchal@gmail.com>
2016-12-24 18:37:54 -0700

    Add connection to database

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var express = require('express')
-var app = express()
+// Dependencies
+var express = require('express');
+var mysql = require('mysql');
+
+// Global Variables
+var app = express();
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  database : 'nodeblog',
+  user     : 'root'
+});
+
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
@@ -10,6 +20,19 @@ app.get('/a', function (req, res) {
   res.send('Something else')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// Database connection script from githum/mysqljs/mysql
+connection.connect(function(err) {
+  if (err) {
+    // Unsuccessful connection to database
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  // Successful to database
+  console.log('connected to database as id ' + connection.threadId);
+
+  // Listen for requests
+  app.listen(3000, function () {
+    console.log('App NodeBlog listening on port 3000!')
+  })
+});

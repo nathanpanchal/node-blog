b85a9f281a56bdb33b6baa171502b47b8ace686e
Nathan Panchal <nathanpanchal@gmail.com>
2016-12-25 11:03:34 -0700

    Add body-parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 // Global Variables
 const app = express();
@@ -10,10 +11,18 @@ const connection = mysql.createConnection({
   user     : 'root'
 });
 
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('Welcome to the NodeBlog app!')
 })
+
+app.post('/newpost',function(req,res){
+  // connection.query('INSERT INTO posts')
+  console.log(req.body);
+  res.send('Post complete');
+});
 
 // Display all the posts in the app.
 app.get('/allposts', function (req, res) {
@@ -23,9 +32,9 @@ app.get('/allposts', function (req, res) {
     // console.log('The solution is: ', rows[0].solution);
     res.send(rows)
   });
-})
+});
 
-// Database connection script from githum/mysqljs/mysql
+// Database connection script from github/mysqljs/mysql
 connection.connect(function(err) {
   if (err) {
     // Unsuccessful connection to database
